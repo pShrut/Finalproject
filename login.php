d3f836836696913309b94daa258d5e063cb257ee
Shruti <sp2372@njit.edu>
2017-12-09 13:36:25 -0500

    second commit

--- login.php
+++ login.php
@@ -5,134 +5,39 @@
    {
      echo 'Register Successfully...';
    }
-  if($_REQUEST['msg']
-																	==
-																									"2")
-																													{
-																																		echo
-																																		'Register
-																																		Not
-																																		Successfully...';
-																																						}
-																																										if($_REQUEST['msg']
-																																										==
-																																										"3")
-																																														{
-																																																			echo
-																																																			'You
-																																																			have
-																																																			alredy
-																																																			register
-																																																			try
-																																																			different
-																																																			email...';
-																																																							}
-																																																											
-																																																															?>
-																																																																		</p>
-																																																																					
-																																																																									<div
-																																																																									class="panel
-																																																																									overflow-hidden">
-																																																																															<div
-																																																																															class="bg-light-blue-500
-																																																																															padding-top-25
-																																																																															no-margin-bottom
-																																																																															font-size-20
-																																																																															color-white
-																																																																															text-center
-																																																																															text-uppercase">
-
-																																																																																						<i
-																																																																																						class="ion-log-in
-																																																																																						margin-right-5"></i>
-																																																																																						Sign
-																																																																																						In
-																																																																																						to
-																																																																																						maniac
-																																																																																												</div>
-																																																																																																		
-																																																																																																						
-																																																																																																										<form
-																																																																																																										id=""
-																																																																																																										method="post"
-																																																																																																										action="#"
-																																																																																																										onsubmit="return
-																																																																																																										chkFrm();">
-																																																																																																															<div
-																																																																																																															class="form-group">
-																																																																																																																					<input
-																																																																																																																					type="email"
-																																																																																																																					name="email"
-																																																																																																																					id="email"
-																																																																																																																					class="form-control
-																																																																																																																					input-lg"
-																																																																																																																					placeholder="Email"
-																																																																																																																					required/>
-																																																																																																																										</div>
-																																																																																																																															
-																																																																																																																																				<div
-																																																																																																																																				class="form-group">
-																																																																																																																																										<input
-																																																																																																																																										type="password"
-																																																																																																																																										name="password"
-																																																																																																																																										id="password"
-																																																																																																																																										class="form-control
-																																																																																																																																										input-lg"
-																																																																																																																																										placeholder="Password"
-																																																																																																																																										required/>
-																																																																																																																																															</div>        
-																																																																																																																																																				
-																																																																																																																																																									       
-																																																																																																																																																									       					<button
-																																																																																																																																																														type="submit"
-																																																																																																																																																														class="btn
-																																																																																																																																																														btn-dark
-																																																																																																																																																														bg-light-green-500
-																																																																																																																																																														padding-10
-																																																																																																																																																														btn-block
-																																																																																																																																																														color-white">Sign
-																																																																																																																																																														in</button>
-
-																																																																																																																																																																			</form>
-																																																																																																																																																																			<!--					<button
-																																																																																																																																																																								type="submit"
-																																																																																																																																																																								class="btn
-																																																																																																																																																																								btn-dark
-																																																																																																																																																																								bg-light-green-500
-																																																																																																																																																																								padding-10
-																																																																																																																																																																								btn-block
-																																																																																																																																																																								color-white"><i
-																																																																																																																																																																								class="ion-log-in"></i>
-																																																																																																																																																																								Sign
-																																																																																																																																																																								in</button>
-																																																																																																																																																																								-->
-																																																																																																																																																																												</div>
-																																																																																																																																																																															
-																																																																																																																																																																																		<div
-																																																																																																																																																																																		class="panel-footer
-																																																																																																																																																																																		padding-md
-																																																																																																																																																																																		no-margin
-																																																																																																																																																																																		no-border
-																																																																																																																																																																																		bg-light-blue-500
-																																																																																																																																																																																		text-center
-																																																																																																																																																																																		color-white">
-																																																																																																																																																																																					<h2>Don't
-																																																																																																																																																																																					Have
-																																																																																																																																																																																					A
-																																																																																																																																																																																					Account</h2>
-																																																																																																																																																																																								<a
-																																																																																																																																																																																								href="index.php?page=account&action=register"
-																																																																																																																																																																																								class="btn
-																																																																																																																																																																																								btn-dark
-																																																																																																																																																																																								bg-light-green-500
-																																																																																																																																																																																								padding-10
-																																																																																																																																																																																								btn-block
-																																																																																																																																																																																								color-white">Sign
-																																																																																																																																																																																								Up
-																																																																																																																																																																																								Now</a>
-																																																																																																																																																																																											
-																																																																																																																																																																																														</div>
-																																																																																																																																																																																																</div>
-																																																																																																																																																																																																	</div>
+  if($_REQUEST['msg'] == "2")
+    {
+    echo 'Register Not Successfully...';
+    }
+   if($_REQUEST['msg'] == "3")
+   {
+     echo 'You have alredy register try different email...';
+     }
+?>
+</p>
+  <div class="panel overflow-hidden">
+    <div class="bg-light-blue-500 padding-top-25 no-margin-bottom font-size-20 color-white text-center text-uppercase">
+    <i class="ion-log-in margin-right-5"></i> Sign In
+   </div>
+   <form id="" method="post" action="#" onsubmit="return chkFrm();">
+   <div class="form-group">
+  <input type="email" name="email" id="email" class="form-control iput-lg" placeholder="Email" required/>
+										</div> 
+										<div class="form-group">
+   <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" required/>
+ </div> 
+  <button type="submit" class="btn btn-dark bg-light-green-500 padding-10 btn-block color-white">Sign in</button>
+  </form>
+  <!--<button type="submit" class="btn btn-dark bg-light-green-500 padding-10
+      btn-block color-white"><i class="ion-log-in"></i>sign in</button>	-->
+      
+      </div>
+      <div class="panel-footer padding-md no-margin no-border bg-light-blue-500
+      text-center color-white"> 
+      <h2>Don't Have A Account</h2>
+      <a href="index.php?page=account&action=register" class="btn btn-dark
+      bg-light-green-500 padding-10 btn-block color-white">Sign up Now</a>
+      </div>
+      </div>
+      </div>
 
